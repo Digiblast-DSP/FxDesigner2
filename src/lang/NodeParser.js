dd1966a58f24b259559a3ba6fe4c13cf01c17044
Matthew Kleitz <kleitz.matthew@gmail.com>
2022-09-27 19:01:15 -0400

    did some work to convert the reactnodes into a directed graph. I should add documentation. Creates a data structure of nodes and references to parent(s).

--- src/lang/NodeParser.js
+++ src/lang/NodeParser.js
@@ -1,21 +1,45 @@
 import FuncData from "./FuncData";
 
-class ASTNode {
-    constructor(parent, func) {
-        this.children = parent;
+class GraphNode {
+    constructor(id, parents, func) {
+        this.parents = parents;
         this.func = func;
+        this.id = id;
     }
 }
 
-class AST {
+class Graph {
     constructor() {
-        this.head = new ASTNode(undefined, undefined);
+        this.head = new GraphNode(-1, undefined, undefined);
+        this.visited = {};
+    }
+
+    traverse(node) {
+        //this.visited = {};
+        console.log("traversing", node.id);
+        this.#multiPreorder(node);
+    }
+
+    #multiPreorder(node) {
+        if (node === undefined) {
+            return;
+        }
+        console.log(node.id);
+
+        if (node.parents.length === undefined) {
+            this.#multiPreorder(undefined);
+        }
+        let inputNodeCount = node.func.paramNames !== undefined ? node.func.paramNames.length : 1;
+
+        for (let i = 0; i < inputNodeCount; i++) {
+            this.#multiPreorder(node.parents[i]);
+        }
     }
 }
 
 export class NodeParser {
     constructor() {
-        this.ast = new AST();
+        this.graph = new Graph();
     }
 
     build(nodes, edges) {
@@ -26,10 +50,36 @@ export class NodeParser {
 
         for (let i = 0; i < nodes.length; i++) {
             const n = nodes[i];
-            nodeDist[n.id] = new ASTNode(undefined, n.data);
+            nodeDist[n.id] = new GraphNode(n.id, {}, n.data);
         }
-        console.table(nodeDist);
+        
 
-        this.ast.head = nodeDist['IN'];
+        this.graph.head = nodeDist['IN'];
+
+        for (let i = 0; i < edges.length; i++) {
+            const e = edges[i];
+            let targetParam = '0';
+            if (e.targetHandle) {
+                const parsedEdgedName = e.targetHandle.split('-');
+                targetParam = parsedEdgedName[parsedEdgedName.length-1];
+            }
+            
+
+            nodeDist[e.target].parents[targetParam] = nodeDist[e.source];
+            
+            /* const source = nodeDist[e.source];
+            if (!e.targetHandle && e.target === 'OUT') {
+                nodeDist[e.source].children[0] = nodeDist['OUT'];
+                continue;
+            }
+            const parsedEdgedName = e.targetHandle.split('-');
+            const targetParam = parsedEdgedName[parsedEdgedName.length-1];
+            nodeDist[e.source].children[targetParam] = nodeDist[e.target]; */
+        }
+        console.log('NODEDIST');
+        console.table(nodeDist);
+        console.log(this.graph.head);
+
+        this.graph.traverse(nodeDist['OUT']);
     }
 }
